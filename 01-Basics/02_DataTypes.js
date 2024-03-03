357053a9168c8b4968716a37d5fdab2cec78e8e7
ArpitBhardwaj7 <109550765+ArpitBhardwaj7@users.noreply.github.com>
2024-03-03 05:52:54 +0000

    Data types

--- 01-Basics/02_DataTypes.js
+++ 01-Basics/02_DataTypes.js
@@ -1,3 +1,27 @@
-"use strict";   // Treat all JavaScript code as newer version code.
+"use strict";   // Treat the JavaScript code as a newer version code.
 
-alert("Hello");   // We are using Node.js not browser.
+// alert("Hello");   // We are using Node.js, not a browser.
+
+/*
+Various Data Types in JavaScript:
+1. number
+2. bigint
+3. string
+4. boolean
+5. null
+6. undefined
+7. symbol
+8. object
+*/
+
+let age = 23;
+let studentName = "Arpit Bhardwaj";
+let loggedIn = true;
+let city;
+let state = null;
+
+console.table([age, studentName, loggedIn, city, state]);
+console.table([typeof(age), typeof(studentName), typeof(loggedIn), typeof(city), typeof(state)]);
+
+console.log(typeof(undefined));   // Output: undefined
+console.log(typeof(null));   // Output: object
