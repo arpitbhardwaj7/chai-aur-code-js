bf75b4f70fd67c01c1332322734e8dba108c6f10
ArpitBhardwaj7 <109550765+ArpitBhardwaj7@users.noreply.github.com>
2024-03-04 09:35:18 +0000

    Conversion operation

---
+++ 01-Basics/04_ConversionOperation-2.js
@@ -0,0 +1,17 @@
+let value = 3;
+let negValue = -value;
+console.log(negValue);   // Output: -3
+
+let str1 = "Hello ";
+let str2 = "Arpit";
+console.log(str1 + str2);   // Output: Hello Arpit
+
+console.log("1" + 2);   // Output: 12
+console.log(1 + "2");   // Output: 12
+console.log("1" + 2 + 2);   // Output: 122
+console.log(1 + 2 + "2");   // Output: 32
+
+console.log(true);   // Output: true
+console.log(false);   // Output: false
+console.log(+true);   // Output: 1
+console.log(+false);   // Output: 0
