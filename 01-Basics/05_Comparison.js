29e4c198fa3bc79d8edfc91ef643d31a14510477
ArpitBhardwaj7 <109550765+ArpitBhardwaj7@users.noreply.github.com>
2024-03-04 09:46:14 +0000

    Comparison

---
+++ 01-Basics/05_Comparison.js
@@ -0,0 +1,17 @@
+// String to Number Conversion
+console.log("2" > 1);   // Output: true
+console.log("02" > 1);   // Output: true
+
+// Equality check (==) and comparison check (>,<,>=,<=) works differently. Comparison converts null to number, treating it as 0.
+console.log(null > 0);   // Output: false
+console.log(null == 0);   // Output: false
+console.log(null >= 0);   // Output: true
+
+console.log(undefined > 0);   // Output: false
+console.log(undefined == 0);   // Output: false
+console.log(undefined >= 0);   // Output: false
+
+// == vs. ===
+// == compares only value, whereas === compares both, value as well as data type.
+console.log("2" == 2);   // Output: true
+console.log("2" === 2);   // Output: false
