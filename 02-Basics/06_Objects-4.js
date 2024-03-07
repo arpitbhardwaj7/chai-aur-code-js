b78f0694cc24981987a7b34d25c9f47643c05b37
ArpitBhardwaj7 <109550765+ArpitBhardwaj7@users.noreply.github.com>
2024-03-07 11:30:00 +0000

    Objects

--- 02-Basics/06_Objects-4.js
+++ 02-Basics/06_Objects-4.js
@@ -1,8 +1,8 @@
-// Object Destructuring
+// Object Destructuring: Object destructuring is a feature in JavaScript that allows you to extract values from objects and assign them to variables in a more concise and readable way.
 student = {
     name: "Arpit Bhardwaj",
     age: 23,
-    college: "IIIT Allahabad",
+    college: "UIT Shimla",
     address: {
         city: "Shimla",
         state: "Himachal Pradesh"
@@ -17,3 +17,7 @@ let {name: studentName, age, college, address} = student;
 console.log(studentName);
 console.log(age);
 console.log(address);
+
+student.college = "IIIT Allahabad";
+console.log(student.college);   // Output: IIIT Allahabad
+console.log(college);   // Output: UIT Shimla
