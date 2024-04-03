d5c1bd491f8e8786d3f95b014f650f588978ae9b
ArpitBhardwaj7 <109550765+ArpitBhardwaj7@users.noreply.github.com>
2024-04-03 03:55:28 +0000

    Promises

--- 08_Advance-1/03_Promises-1.js
+++ 08_Advance-1/03_Promises-1.js
@@ -1,4 +1,11 @@
-// Promise: In JavaScript, a promise represents a value that may not be available yet but will be resolved at some point in the future, either with a value or with an error.
+/*
+Promise: In JavaScript, a promise represents a value that may not be available yet but will be resolved at some point in the future, either with a value or with an error.
+
+A Promise is in one of these states:
+1. pending: initial state, neither fulfilled nor rejected.
+2. fulfilled: meaning that the operation was completed successfully.
+3. rejected: meaning that the operation failed.
+*/
 
 const promiseObj = new Promise(function (resolve, reject) {
     // Perform asynchronous operations.
